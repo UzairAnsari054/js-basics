bde6a75711dc02bc0c16a96524c9dc20031fe731
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-12 20:43:33 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,6 +27,33 @@
  "Uzair" => true
  */
 
- let rollNumber = 12
+ let rollNumber = 33
  let StringNumber = String(rollNumber)
- console.log(typeof(StringNumber));
+ console.log(StringNumber);
+/* eg. Converting in String:
+ 33 => 33 as type string
+ true => true as type string
+ */
+
+ // OPERATION
+
+ let value = 3
+ let negValue = -value
+ console.log(negValue);
+
+console.log(2%2); // Remainder
+console.log(2/2); // Quotient
+
+let str1 = "Hello"
+let str2 = " Uzair"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2"); // => 12 
+console.log("1" + "2"); // => 12
+console.log("1" + 2 + 2); // => 122
+console.log(1 + 2 + "2"); // => 32
+
+console.log(+true) // => 1
+console.log(+"") // => 0
