241b9efba4f2f63220c210ce348b93596e5804f0
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-12 22:04:54 +0000

    Memory(Stack & Heap)

--- 01_basics/datatypes-summaries.js
+++ 01_basics/datatypes-summaries.js
@@ -1,9 +1,11 @@
 // Primitive data types (Call by value)
 // Primitive data types are stored directly in memory, for eg myString = "Uzair", so myString will be stored in the memory
+// In primitive data types, changing a variable's value creates a new copy with the updated value.
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 // Non Primitive data type (Call by reference)
 // Non-primitive data types are stored as references to their values in memory. for eg, myArray = [a, b, c], so myArray will be stored in the memory but as a reference to [a, b, c]
+// In Non-primitive data types, changing a variable's value updates the orignal value.
 // 7 types : Array, Objects, Functions
 
 const id = Symbol('123')
@@ -41,3 +43,25 @@ console.log(typeof myObj); // => object
 console.log(typeof myFunction); // => object function
 
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//        (2 types of Memory in js)
+
+// 1. Stack (Primitive data types)
+//     - In Stack memory, you get the copy of variable's value & changing a variable's value creates a new copy with the updated value.
+let myName = "Uzair"
+let otherName = myName
+otherName = "Sonu"
+
+
+// 2. Heap (Non-primitive data types)
+//     - In Heap memory, you get the reference of original value & changing a variable's value updates the orignal value.
+let user1 = {
+    email: "user1@google.com",
+    upi: "user1"
+}
+let user2 = user1
+user2.email = "user2@google.com"
+
+console.log(user1); // => { email: 'user2@google.com', upi: 'user1' }
+console.log(user2); // => { email: 'user2@google.com', upi: 'user1' }
