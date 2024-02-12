d62cd4728664d873895ca2d581556a19608117fc
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-12 22:43:38 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+let name = "Uzair-An"
+let age = 25
+
+console.log(`My name is ${name} & my age is ${age}`);
+console.log(name[0]);
+console.log(name.__proto__);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt('3'));
+console.log(name.indexOf('r'));
+console.log(name.substring(0,3))
+console.log(name.slice(-2,1));
+console.log();
+
+// In a browser open Inspect -> Console & enter, let name = new String('Uzair') then again enter name to get all string functions. To understand the functions take help from MDN
