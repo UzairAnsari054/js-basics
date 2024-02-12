7d7fdc16f3559bb8d8aef737f23ee50d9ba3d112
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-12 19:39:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "uzair@google.com"
+var accounfPassword = "12345"
+accountCity = "Jaiput"
+let accountState
+
+//accountId = 2
+accountEmail = "hc@hc.com"
+accounfPassword = "212121"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accounfPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) alert is not defined because we are using nodejs, not browser
+
+let name = "uzair"
+let age = 25
+let isLoggedIn = true
+
+// null => stand alone value, In Js null type is object
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
