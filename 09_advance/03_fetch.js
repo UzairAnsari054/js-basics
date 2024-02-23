6e924ec332a559cb2a81b051c4c4cdbb5c478a80
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-02-23 03:38:39 +0000

    Promise

---
+++ 09_advance/03_fetch.js
@@ -0,0 +1,5 @@
+// The global fetch() method starts the process of fetching a resource from the network, returning a promise which is fulfilled once the response is available.
+// reject() will only be the case when the request couldn't be made. Even if yiu get an error as response, it will still be treated under resolve()
+
+// CHECK FROM 6:31:00 - 6:39:00
+//  https://www.youtube.com/watch?v=_TjtAyMkiTI
